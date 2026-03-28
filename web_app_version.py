301822d0aad398cc6e6adc2dd990dcaa9e2fc93a
MPAGI SHAFIQ <shafiqmpagi@gmail.com>
2026-03-28 17:59:49 +0300

    Refactor web app version with model loading and UI

--- web_app_version.py
+++ web_app_version.py
@@ -109,6 +109,6 @@ hide_streamlit_style = """
                         header {visibility:hidden;}
                         footer {visibility:hidden;}
                         .stAppDeployButton {display:none;}
-                        <style>
+                        </style>
                       """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
